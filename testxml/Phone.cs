84aad70d52ed47285995bf32e88cd60294fff6df
makskayun <20Nebeyvspinu00>
2021-12-30 14:02:27 +0300

    init

---
+++ testxml/Phone.cs
@@ -0,0 +1,10 @@
+﻿namespace testxml
+{
+    public class Phone
+    {
+        public string Name { get; set; }
+        public int Number { get; set; }
+        public string Remark { get; set; }
+        public bool Important { get; set; }
+    }
+}
